a82c6652bad3029a366a36e775c14c504cb7cd2c
carlos soliz <carlos.soliz.t1vl@gmail.com>
2024-11-08 13:17:07 -0300

    cambios nuevos 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './assets/global.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './assets/global.css';
+import App from './App.jsx';
 import { obtenerDatosJWT } from './lib/obtenerDatosJWT.js';
 
-window.addEventListener("message", (event) => {
-  // Asegúrate de que el mensaje proviene del iframe
-  if (event.source !== document.getElementById("miIframe").contentWindow) return;
+// Asegúrate de que el evento de mensaje se registre solo después de que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("message", (event) => {
+    const iframe = document.getElementById("miIframe");
 
-  const { type } = event.data;
+    // Verifica si el iframe existe y que el mensaje provenga de su contentWindow
+    if (!iframe || event.source !== iframe.contentWindow) return;
 
-  if (type === "getLocalStorage") {
-    // Obtiene el valor del localStorage de la página principal
-    const value = obtenerDatosJWT();
+    const { type } = event.data;
 
-    // Envía el valor de vuelta al iframe
-    event.source.postMessage({ type: "localStorageValue", value }, event.origin);
-  }
+    if (type === "getLocalStorage") {
+      // Obtiene el valor del localStorage de la página principal
+      const value = obtenerDatosJWT();
+
+      // Envía el valor de vuelta al iframe
+      event.source.postMessage({ type: "localStorageValue", value }, event.origin);
+    }
+  });
 });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
