1e0b9a62f45bb47ff521a0d7884df49d4bcaf085
carlos soliz <carlos.soliz.t1vl@gmail.com>
2024-12-06 14:13:15 -0300

    cambio 2.5

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SideBar from './components/SideBar.jsx';
 import VerificarPaso from './lib/verificarPaso.js';
 
-import ShowFrame from './Routes/ShowFrame.jsx';
-import Login from './Routes/Login.jsx';
+import ShowFrame from './routes/ShowFrame.jsx';
+import Login from './routes/Login.jsx';
 import NavBar from './components/NavBar.jsx';
-import Install from './Routes/Install.jsx';
-import Home from './Routes/Home.jsx';
+import Install from './routes/Install.jsx';
+import Home from './routes/Home.jsx';
 
 function App() {
   const [step, setStep] = useState(sessionStorage.getItem("step"));
@@ -19,12 +19,6 @@ function App() {
       setStep(x);
     });
   }, []);
-
-
-  const hanlderInitDownload = (e) => {
-    e.preventDefault();
-    sessionStorage.step = 0;
-  };
 
   if (step === null) {
     return <></>;
